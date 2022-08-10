28df5bafec8b42976e2bd777053c1b9246afff13
Emanuel Massafera <emanuel301@live.com>
2022-08-10 16:08:56 -0300

    chore: add docker-compose

--- node/index.js
+++ node/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
+
 const port = process.env.PORT || 3000;
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+};
+
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Emanuel')`;
+
+connection.query(sql);
+connection.end();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
